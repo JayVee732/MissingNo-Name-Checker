8ca0e80b0690c5b3126c2c2aaf6fdad201a4dfa7
Jamie Higgins <jamie.v732@gmail.com>
2017-06-01 23:29:53 +0100

    01/06/2017 23:25
    XAML
    +Added two more buttons, for special characters in Pokémon Red and Blue
    + Help button added, more info can be put in there at a later point
    + Change from one textblock to two, to help seperate Pokémon and Levels
    
    MainWindow.xaml.cs
    + Added comments to most parts, more to be done
    + If there is less than 7 characters in the textbox, the program will fill in the rest with spaces
    + Changes variable names in SearchFiles method
    
    Textfiles
    + Added ability for more characters

--- MissingNo-Name-Checker/MissingNo-Name-Checker/MainWindow.xaml.cs
+++ MissingNo-Name-Checker/MissingNo-Name-Checker/MainWindow.xaml.cs
@@ -22,7 +22,8 @@ namespace MissingNo_Name_Checker
     /// </summary>
     public partial class MainWindow : Window
     {
-        char[] nameInput = new char[7];
+        //The users name is stored here as 7 seperate characters
+        char[] nameArray = new char[7];
         public MainWindow()
         {
             InitializeComponent();
@@ -30,63 +31,121 @@ namespace MissingNo_Name_Checker
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            //A players name in Gen I can only be 7 characters long
             userNameInput.MaxLength = 7;
         }
 
         private void userNameInput_GotFocus(object sender, RoutedEventArgs e)
         {
+            //When the textbox is clicked, the contents of it are cleared
             userNameInput.Text = "";
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void btnGenerate_Click(object sender, RoutedEventArgs e)
         {
-            nameInput = userNameInput.Text.ToCharArray();
-            
-            tblkResult.Text = "";
+            AddValuesToArray();
 
             try
             {
+                //2 Different files are used
                 using (StreamReader sr = new StreamReader("levelList.txt"))
                 {
-                    const int LEVELSEARCH = 1;
-                    SearchFiles(sr, LEVELSEARCH);
+                    //const value used as it will never change
+                    const int LEVEL_SEARCH = 1;
+                    SearchFiles(sr, LEVEL_SEARCH);
                 }
 
                 using (StreamReader sr = new StreamReader("pokemonList.txt"))
                 {
-                    const int POKEMONSEARCH = 2;
-                    SearchFiles(sr, POKEMONSEARCH);
+                    const int POKEMON_SEARCH = 2;
+                    SearchFiles(sr, POKEMON_SEARCH);
                 }
             }
             catch (Exception ex)
             {
+                //If there's a problem
                 MessageBox.Show(ex.Message);
             }
         }
 
-        private void SearchFiles(StreamReader sr, int SEARCHBY)
+        private void AddValuesToArray()
         {
-            for (int i = SEARCHBY; i < nameInput.Length; i += 2)
+            if (userNameInput.Text.Length < userNameInput.MaxLength)
             {
-                //next line
-                string contents = sr.ReadLine();
-                while (contents != null)
+                for (int i = 0; i < userNameInput.MaxLength; i++)
                 {
-                    //get level that goes with letter
-                    string[] newLevel = contents.Split(',');
-                    string levelDisplay = newLevel[1];
-
-                    if (newLevel[0] == nameInput[i].ToString())
+                    userNameInput.Text += " ";
+                    if (userNameInput.Text.Length == userNameInput.MaxLength)
                     {
-                        tblkResult.Text += levelDisplay.ToString();
-                        newLevel = null;
+                        break;
+                    }
+                }
+            }
+            //Textbox is converted to char array
+            nameArray = userNameInput.Text.ToCharArray();
+            //Cleared if there was a previous search
+            tblkLevel.Text = "";
+            tblkPokemon.Text = "";
+        }
+
+        private void SearchFiles(StreamReader sr, int SEARCH_BY)
+        {
+            //Starts on the const value and increments by 2 each time for every even or odd number
+            for (int i = SEARCH_BY; i < nameArray.Length; i += 2)
+            {
+                //Reads the first line
+                string contents = sr.ReadLine();
+                while (contents != null) //Keeps reading until the end of the file
+                {
+                    //Splits the line from the textfile and reads the first value
+                    string[] newLine = contents.Split('|');
+                    string checkLine = newLine[1];
+
+                    if (newLine[0] == nameArray[i].ToString())
+                    {
+                        //Appends to textbox depending on if it's the level or Pokemon file
+                        if (SEARCH_BY == 2)
+                        {
+                            tblkPokemon.Text += String.Format("\n{0}", checkLine.ToString());
+                        }
+                        else
+                        {
+                            tblkLevel.Text += String.Format("\n{0}", checkLine.ToString());
+                        }
+                        newLine = null;
+                        //Starts reading the text file from the start for the next character
                         sr.DiscardBufferedData();
                         sr.BaseStream.Seek(0, SeekOrigin.Begin);
                         break;
                     }
+                    //Reads the next line
                     contents = sr.ReadLine();
                 }
             }
         }
+#region Additional Buttons
+        private void PKButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (userNameInput.Text.Length < userNameInput.MaxLength)
+            {
+                userNameInput.Text += "#";
+            }
+        }
+
+        private void MNButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (userNameInput.Text.Length < userNameInput.MaxLength)
+            {
+                userNameInput.Text += "\\";
+            }
+        }
+        private void btnHelp_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hi there, and thanks for using this program! ^_^" +
+                "\n\nThe purpose of this application is to allow you to enter your trainer name from Pokémon Red and Blue and see what Pokémon (or trainers) you'll get from the Old Man Glitch." +
+                "\n\nThe PK and MN buttons are for the extra characters in the games that aren't on a normal keyboard. They are represented as '#' and '\\' respectively in the program, however, they will still produce the same results as if they were the PK and MN values." +
+                "\n\n- Jamie");
+        }
+        #endregion
     }
 }
