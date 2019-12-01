fe23b0a9373b30a461b69263471baea84f17a915
PsychoHacks <jamie.v732@gmail.com>
2019-12-01 01:20:39 +0000

    Massive rewrite of core logic to remove reading from text files Dictionaries not finished

--- MissingNo-Name-Checker/MainWindow.xaml.cs
+++ MissingNo-Name-Checker/MainWindow.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.IO;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using System.Windows;
 
@@ -10,8 +10,94 @@ namespace MissingNo_Name_Checker
     /// </summary>
     public partial class MainWindow : Window
     {
-        //The users name is stored here as 7 seperate characters
+        public readonly IDictionary<string, int> levelList = new Dictionary<string, int>()
+        {
+            { string.Empty, 127 },
+            { "A",  128 },
+            { "B", 129 },
+            { "C", 130 },
+            { "D", 131 },
+            { "E", 132 },
+        };
+
+        public readonly IDictionary<string, string> pokemonList = new Dictionary<string, string>()
+        {
+            { string.Empty, "MissingNo." },
+            { "A", "Golduck" },
+            { "B", "Hypno" },
+            { "C", "Golbat" },
+            { "D", "Mewtwo" },
+            { "E", "Snorlax" },
+            { "F", "MissingNo." },
+            { "G", "MissingNo." },
+            { "H", "MissingNo." },
+            { "I", "MissingNo." },
+            { "J", "MissingNo." },
+            { "K", "MissingNo." },
+            { "L", "MissingNo." },
+            { "M", "MissingNo." },
+            { "N", "MissingNo." },
+            { "O", "MissingNo." },
+            { "P", "MissingNo." },
+            { "Q", "MissingNo." },
+            { "R", "MissingNo." },
+            { "S", "MissingNo." },
+            { "T", "MissingNo." },
+            { "U", "MissingNo." },
+            { "V", "MissingNo." },
+            { "W", "MissingNo." },
+            { "X", "MissingNo." },
+            { "Y", "MissingNo." },
+            { "Z", "MissingNo." },
+            { "(", "MissingNo." },
+            { ")", "MissingNo." },
+            { ":", "MissingNo." },
+            { ";", "MissingNo." },
+            { "[", "MissingNo." },
+            { "]", "MissingNo." },
+            { "a", "MissingNo." },
+            { "b", "MissingNo." },
+            { "c", "MissingNo." },
+            { "d", "MissingNo." },
+            { "e", "MissingNo." },
+            { "f", "MissingNo." },
+            { "g", "MissingNo." },
+            { "h", "MissingNo." },
+            { "i", "MissingNo." },
+            { "j", "MissingNo." },
+            { "k", "MissingNo." },
+            { "l", "MissingNo." },
+            { "m", "MissingNo." },
+            { "n", "MissingNo." },
+            { "o", "MissingNo." },
+            { "p", "MissingNo." },
+            { "q", "MissingNo." },
+            { "r", "MissingNo." },
+            { "s", "MissingNo." },
+            { "t", "MissingNo." },
+            { "u", "MissingNo." },
+            { "v", "MissingNo." },
+            { "w", "MissingNo." },
+            { "x", "MissingNo." },
+            { "y", "MissingNo." },
+            { "z", "MissingNo." },
+            { "#", "MissingNo." },
+            { "\\", "MissingNo." },
+            { "-", "MissingNo." },
+            { "?", "MissingNo." },
+            { "!", "MissingNo." },
+            { "♂", "MissingNo." },
+            { "*", "MissingNo." },
+            { ".", "MissingNo." },
+            { "/", "MissingNo." },
+            { ",", "MissingNo." },
+            { "♀", "MissingNo." },
+        };
+
         char[] nameArray = new char[7];
+        const int LEVEL_SEARCH = 1;
+        const int POKEMON_SEARCH = 2;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -19,115 +105,75 @@ namespace MissingNo_Name_Checker
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            //A players name in Gen I can only be 7 characters long
+            // A players name in Gen I can only be 7 characters long
             userNameInput.MaxLength = 7;
         }
 
         private void userNameInput_GotFocus(object sender, RoutedEventArgs e)
         {
-            //When the textbox is clicked, the contents of it are cleared
+            // When the textbox is clicked, the contents of it are cleared
             userNameInput.Text = "";
         }
 
         private void btnGenerate_Click(object sender, RoutedEventArgs e)
         {
-            AddValuesToArray();
-
             try
             {
-                //2 Different files are used
-                using (StreamReader sr = new StreamReader("data/levelList.txt"))
-                {
-                    //const value used as it will never change
-                    const int LEVEL_SEARCH = 1;
-                    SearchFiles(sr, LEVEL_SEARCH);
-                }
-
-                using (StreamReader sr = new StreamReader("data/pokemonList.txt"))
-                {
-                    const int POKEMON_SEARCH = 2;
-                    SearchFiles(sr, POKEMON_SEARCH);
-                }
+                AddValuesToArray();
+                SearchLists(POKEMON_SEARCH);
+                SearchLists(LEVEL_SEARCH);
             }
             catch (Exception ex)
             {
-                //If there's a problem
                 MessageBox.Show(ex.Message);
             }
         }
 
-        //Adds more spacing to the end of the characters name, the games did that same thing
+        // Adds more spacing to the end of the characters name, the games did that same thing
         private void AddValuesToArray()
         {
-            if (userNameInput.Text.Length < userNameInput.MaxLength)
-            {
-                for (int i = 0; i < userNameInput.MaxLength; i++)
-                {
-                    userNameInput.Text += " ";
-                    if (userNameInput.Text.Length == userNameInput.MaxLength)
-                    {
-                        //Leave the loop
-                        break;
-                    }
-                }
-            }
+            userNameInput.Text.PadRight(userNameInput.MaxLength - userNameInput.Text.Length);
 
             ValidateUsername();
 
-            //Cleared if there was a previous search
+            // Cleared if there was a previous search
             tblkLevel.Text = "";
             tblkPokemon.Text = "";
         }
 
-        //Validate the user input
+        // Validate the user input
         private void ValidateUsername()
         {
-            //Regex that should cover all name combinations
+            // Regex that should cover all name combinations
             Regex regex = new Regex(@"^[a-zA-Z\s\#\\\-\?\!\*\.\/\♀\♂]{1,7}$");
             Match match = regex.Match(userNameInput.Text);
-            if (match.Success)
-            {
-                //Stores the result
-                nameArray = userNameInput.Text.ToCharArray();
-            }
-            else
+            if (!match.Success)
             {
                 MessageBox.Show("Invalid characters where found in your username, please try again!");
             }
+
+            nameArray = userNameInput.Text.ToCharArray();
         }
 
-        private void SearchFiles(StreamReader sr, int SEARCH_BY)
+        private void SearchLists(int SEARCH_BY)
         {
-            //Starts on the const value and increments by 2 each time for every even or odd number
+            // Starts on the const value and increments by 2 each time for every even or odd number
             for (int i = SEARCH_BY; i < nameArray.Length; i += 2)
             {
-                //Reads the first line
-                string contents = sr.ReadLine();
-                while (contents != null) //Keeps reading until the end of the file
+                // Pokémon
+                if (SEARCH_BY % 2 == 0)
                 {
-                    //Splits the line from the textfile and reads the first value
-                    string[] newLine = contents.Split('|');
-                    string checkLine = newLine[1];
-
-                    if (newLine[0] == nameArray[i].ToString())
+                    if (pokemonList.TryGetValue(nameArray[i].ToString(), out string pokemon))
                     {
-                        //Appends to textbox depending on if it's the level or Pokemon file
-                        if (SEARCH_BY == 2)
-                        {
-                            tblkPokemon.Text += $"\n{checkLine}";
-                        }
-                        else
-                        {
-                            tblkLevel.Text += $"\n{checkLine}";
-                        }
-                        newLine = null;
-                        //Starts reading the text file from the start for the next character
-                        sr.DiscardBufferedData();
-                        sr.BaseStream.Seek(0, SeekOrigin.Begin);
-                        break;
+                        tblkPokemon.Text += $"\n{pokemon}";
                     }
-                    //Reads the next line
-                    contents = sr.ReadLine();
+                }
+                else
+                {
+                    if (levelList.TryGetValue(nameArray[i].ToString(), out int level))
+                    {
+                        tblkLevel.Text += $"\n{level}";
+                    }
                 }
             }
         }
