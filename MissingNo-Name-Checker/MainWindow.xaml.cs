c0c2fbf02d5c99883312b56fa6ca6c32e3222149
PsychoHacks <jamie.v732@gmail.com>
2019-12-12 23:16:59 +0000

    Core logic is now fully functional. Default values are displayed when the character is blank Move to .NET Core 3.1

--- MissingNo-Name-Checker/MainWindow.xaml.cs
+++ MissingNo-Name-Checker/MainWindow.xaml.cs
@@ -10,89 +10,155 @@ namespace MissingNo_Name_Checker
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Dictionaries
         public readonly IDictionary<string, int> levelList = new Dictionary<string, int>()
         {
-            { string.Empty, 127 },
+            { " ", 127 },
             { "A",  128 },
             { "B", 129 },
             { "C", 130 },
             { "D", 131 },
             { "E", 132 },
+            { "F", 133 },
+            { "G", 134 },
+            { "H", 135 },
+            { "I", 136 },
+            { "J", 137 },
+            { "K", 138 },
+            { "L", 139 },
+            { "M", 140 },
+            { "N", 141 },
+            { "O", 142 },
+            { "P", 143 },
+            { "Q", 144 },
+            { "R", 145 },
+            { "S", 146 },
+            { "T", 147 },
+            { "U", 148 },
+            { "V", 149 },
+            { "W", 150 },
+            { "X", 151 },
+            { "Y", 152 },
+            { "Z", 153 },
+            { "(", 154 },
+            { ")", 155 },
+            { ":", 156 },
+            { ";", 157 },
+            { "[", 158 },
+            { "]", 159 },
+            { "a", 160 },
+            { "b", 161 },
+            { "c", 162 },
+            { "d", 163 },
+            { "e", 164 },
+            { "f", 165 },
+            { "g", 166 },
+            { "h", 167 },
+            { "i", 168 },
+            { "j", 169 },
+            { "k", 170 },
+            { "l", 171 },
+            { "m", 172 },
+            { "n", 173 },
+            { "o", 174 },
+            { "p", 175 },
+            { "q", 176 },
+            { "r", 177 },
+            { "s", 178 },
+            { "t", 179 },
+            { "u", 180 },
+            { "v", 181 },
+            { "w", 182 },
+            { "x", 183 },
+            { "y", 184 },
+            { "z", 185 },
+            { "#", 225 },
+            { "\\", 226 },
+            { "-", 227 },
+            { "?", 230 },
+            { "!", 231 },
+            { "♂", 239 },
+            { "*", 241 },
+            { ".", 242 },
+            { "/", 243 },
+            { ",", 244 },
+            { "♀", 245 },
         };
 
         public readonly IDictionary<string, string> pokemonList = new Dictionary<string, string>()
         {
-            { string.Empty, "MissingNo." },
+            { " ", "MissingNo." },
             { "A", "Golduck" },
             { "B", "Hypno" },
             { "C", "Golbat" },
             { "D", "Mewtwo" },
             { "E", "Snorlax" },
-            { "F", "MissingNo." },
+            { "F", "Magikarp" },
             { "G", "MissingNo." },
             { "H", "MissingNo." },
-            { "I", "MissingNo." },
+            { "I", "Muk" },
             { "J", "MissingNo." },
-            { "K", "MissingNo." },
-            { "L", "MissingNo." },
+            { "K", "Kingler" },
+            { "L", "Cloyster" },
             { "M", "MissingNo." },
-            { "N", "MissingNo." },
-            { "O", "MissingNo." },
-            { "P", "MissingNo." },
-            { "Q", "MissingNo." },
-            { "R", "MissingNo." },
+            { "N", "Electrode" },
+            { "O", "Clefable" },
+            { "P", "Weezing" },
+            { "Q", "Persian" },
+            { "R", "Marowak" },
             { "S", "MissingNo." },
-            { "T", "MissingNo." },
-            { "U", "MissingNo." },
-            { "V", "MissingNo." },
-            { "W", "MissingNo." },
-            { "X", "MissingNo." },
-            { "Y", "MissingNo." },
-            { "Z", "MissingNo." },
-            { "(", "MissingNo." },
-            { ")", "MissingNo." },
+            { "T", "Haunter" },
+            { "U", "Abra" },
+            { "V", "Alakazam" },
+            { "W", "Pidgeotto" },
+            { "X", "Pidgeot" },
+            { "Y", "Starmie" },
+            { "Z", "Bulbasaur" },
+            { "(", "Venusaur" },
+            { ")", "Tentacruel" },
             { ":", "MissingNo." },
-            { ";", "MissingNo." },
-            { "[", "MissingNo." },
+            { ";", "Goldeen" },
+            { "[", "Seaking" },
             { "]", "MissingNo." },
             { "a", "MissingNo." },
             { "b", "MissingNo." },
             { "c", "MissingNo." },
-            { "d", "MissingNo." },
-            { "e", "MissingNo." },
-            { "f", "MissingNo." },
-            { "g", "MissingNo." },
-            { "h", "MissingNo." },
-            { "i", "MissingNo." },
-            { "j", "MissingNo." },
-            { "k", "MissingNo." },
-            { "l", "MissingNo." },
+            { "d", "Ponyta" },
+            { "e", "Rapidash" },
+            { "f", "Rattata" },
+            { "g", "Raticate" },
+            { "h", "Nidorino" },
+            { "i", "Nidorina" },
+            { "j", "Geodude" },
+            { "k", "Porygon" },
+            { "l", "Aerodactyl" },
             { "m", "MissingNo." },
-            { "n", "MissingNo." },
+            { "n", "Magnemite" },
             { "o", "MissingNo." },
             { "p", "MissingNo." },
-            { "q", "MissingNo." },
-            { "r", "MissingNo." },
-            { "s", "MissingNo." },
-            { "t", "MissingNo." },
-            { "u", "MissingNo." },
+            { "q", "Charmander" },
+            { "r", "Squirtle" },
+            { "s", "Charmeleon" },
+            { "t", "Wartortle" },
+            { "u", "Charizard" },
             { "v", "MissingNo." },
-            { "w", "MissingNo." },
-            { "x", "MissingNo." },
-            { "y", "MissingNo." },
-            { "z", "MissingNo." },
-            { "#", "MissingNo." },
-            { "\\", "MissingNo." },
-            { "-", "MissingNo." },
-            { "?", "MissingNo." },
-            { "!", "MissingNo." },
-            { "♂", "MissingNo." },
-            { "*", "MissingNo." },
-            { ".", "MissingNo." },
-            { "/", "MissingNo." },
-            { ",", "MissingNo." },
-            { "♀", "MissingNo." },
+            { "w", "MissingNo. (Kabutops Fossil form)" },
+            { "x", "MissingNo. (Aerodactyl Fossil form)" },
+            { "y", "MissingNo. (Ghost form)" },
+            { "z", "Oddish" },
+            { "#", "Rival Blue" },
+            { "\\", "Professor Oak" },
+            { "-", "Chief" },
+            { "?", "Rocket" },
+            { "!", "Cooltrainer" },
+            { "♂", "Blaine" },
+            { "*", "Gentleman" },
+            { ".", "Rival Blue" },
+            { "/", "Champion Blue" },
+            { ",", "Lorelei" },
+            { "♀", "Channeler" },
         };
+        #endregion
 
         char[] nameArray = new char[7];
         const int LEVEL_SEARCH = 1;
@@ -119,7 +185,7 @@ namespace MissingNo_Name_Checker
         {
             try
             {
-                AddValuesToArray();
+                string newUsername = AddValuesToArray();
                 SearchLists(POKEMON_SEARCH);
                 SearchLists(LEVEL_SEARCH);
             }
@@ -129,39 +195,53 @@ namespace MissingNo_Name_Checker
             }
         }
 
-        // Adds more spacing to the end of the characters name, the games did that same thing
-        private void AddValuesToArray()
+        /// <summary>
+        /// Adds more spacing to the end of the characters name, the games did that same thing.
+        /// </summary>
+        /// <returns>string containing the username.</returns>
+        private string AddValuesToArray()
         {
-            userNameInput.Text.PadRight(userNameInput.MaxLength - userNameInput.Text.Length);
+            string newUsername = userNameInput.Text.PadRight(userNameInput.MaxLength);
 
-            ValidateUsername();
+            ValidateUsername(ref newUsername);
 
             // Cleared if there was a previous search
             tblkLevel.Text = "";
             tblkPokemon.Text = "";
+
+            return newUsername;
         }
 
-        // Validate the user input
-        private void ValidateUsername()
+        /// <summary>
+        /// Validate the user input.
+        /// </summary>
+        /// <param name="newUsername">The Trainer name.</param>
+        private void ValidateUsername(ref string newUsername)
         {
             // Regex that should cover all name combinations
             Regex regex = new Regex(@"^[a-zA-Z\s\#\\\-\?\!\*\.\/\♀\♂]{1,7}$");
-            Match match = regex.Match(userNameInput.Text);
-            if (!match.Success)
+            Match match = regex.Match(newUsername);
+            if (match.Success)
+            {
+                nameArray = newUsername.ToCharArray();
+            }
+            else
             {
                 MessageBox.Show("Invalid characters where found in your username, please try again!");
             }
-
-            nameArray = userNameInput.Text.ToCharArray();
         }
 
+        /// <summary>
+        /// Search for the values in the dictionaries.
+        /// </summary>
+        /// <param name="SEARCH_BY">Determines which dictionary to look through.</param>
         private void SearchLists(int SEARCH_BY)
         {
             // Starts on the const value and increments by 2 each time for every even or odd number
             for (int i = SEARCH_BY; i < nameArray.Length; i += 2)
             {
                 // Pokémon
-                if (SEARCH_BY % 2 == 0)
+                if (SEARCH_BY == 2)
                 {
                     if (pokemonList.TryGetValue(nameArray[i].ToString(), out string pokemon))
                     {
